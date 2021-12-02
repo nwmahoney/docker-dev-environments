588600a58bbb147a31fa5e01de87c03de477138a
Nick Mahoney <nick.mahoney00@gmail.com>
2021-12-02 15:58:01 -0800

    Fix `set_pipeline.sh` script.

--- scripts/set_pipeline.sh
+++ scripts/set_pipeline.sh
@@ -6,9 +6,9 @@ function main {
   fly -t tutorial set-pipeline \
     --var="private-key=$(cat ~/.ssh/id_ed25519)" \
     --var="private-key-passphrase=$(op get item 'ssh passphrase (Linux Mint)' | jq -r .details.password)" \
-    --var="docker-password=$(op get item Docker | jq -r '.details.fields | .[] |  select(.designation == \"password\") | .value')" \
+    --var="docker-password=$(op get item Docker | jq -r '.details.fields | .[] |  select(.designation == "password") | .value')" \
     -p docker-dev-environments \
-    -c docker-dev-environments.yml
+    -c ci/pipelines/docker-dev-environments.yml
 }
 
 main "$@"
